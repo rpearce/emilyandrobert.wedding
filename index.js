06440e79c4b29dcc9edb65d619e548ffeb3ff6a1
Robert Pearce <me@robertwpearce.com>
2017-04-12 16:24:46 +1200

    Better building. Got sass in there

--- index.js
+++ index.js
@@ -2,16 +2,24 @@
 
 const path = require('path')
 const { copy, rmDir, mkDir, writeFile } = require('./lib/fs')
+const { buildSass } = require('./lib/sass')
 const markup = require('./src/index')
 
-const outputPath = path.join(__dirname, 'build')
-const srcPath = path.join(__dirname, 'src')
+const outputDir = path.join(__dirname, 'build')
+const htmlOutputPath = path.join(outputDir, 'index.html')
+const srcDir = path.join(__dirname, 'src')
+const sassDir = path.join(srcDir, 'sass')
+const sassEntry = path.join(sassDir, 'app.scss')
+const cssOutputPath = path.join(outputDir, 'styles.css')
+const cnamePath = path.join(srcDir, 'CNAME')
+const cnameOutputPath = path.join(outputDir, 'CNAME')
 
-rmDir(outputPath)
-  .chain(_ => mkDir(outputPath))
-  .chain(_ => copy(path.join(srcPath, 'CNAME'), path.join(outputPath, 'CNAME')))
-  .chain(_ => copy(path.join(srcPath, 'styles.css'), path.join(outputPath, 'styles.css')))
-  .chain(_ => writeFile(path.join(outputPath, 'index.html'), markup()))
+rmDir(outputDir)
+  .chain(_ => mkDir(outputDir))
+  .chain(_ => copy(cnamePath, cnameOutputPath))
+  .chain(_ => buildSass({ sassDir, sassEntry }))
+  .chain(css => writeFile(cssOutputPath, css))
+  .chain(_ => writeFile(htmlOutputPath, markup()))
   .fork(
     err => console.error('Error: ', err),
     data => console.log(data || 'Build succeeded.')
