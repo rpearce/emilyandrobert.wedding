f2919f565e655bf0e053e818028420863b4cbb45
Robert Pearce <me@robertwpearce.com>
2017-04-23 15:12:15 +1200

    venue

--- src/js/components/home.js
+++ src/js/components/home.js
@@ -18,44 +18,54 @@ function home() {
         </div>
       </header>
       <section>
-        <img src="images/kumeu.jpg" alt="Kumeu Valley Estate" />
-        <h2>Kumeu Valley Estate</h3>
-        <ul>
-          <li>
-            <a href="https://www.kumeuvalley.co.nz/">
-              Website
-            </a>
-          </li>
-          <li>
-            <a href="https://www.google.co.nz/maps/place/Kumeu+Valley+Estate/@-36.74204,174.5089526,17z/data=!4m13!1m7!3m6!1s0x6d0d13e2a8bebcf9:0x72cb9cd0bdfe7b12!2s972+Old+N+Rd,+Huapai,+Waimauku+0882!3b1!8m2!3d-36.74204!4d174.5111413!3m4!1s0x6d0d13e2a8bebcf9:0x78e8ec38867eff09!8m2!3d-36.74204!4d174.5111413">
-              972 Old North Road, Waimauku, Auckland
-            </a>
-          </li>
-        </ul>
-      </section>
-      <section>
-        <h2>Flight Information</h2>
-      </section>
-      <section>
-        <h2>Car Rental</h2>
-      </section>
-      <section>
-        <h2>Auckland Accomodation</h2>
-      </section>
-      <section>
-        <h2>Things To Do</h2>
-        <div>
-          <h3>Around Auckland</h3>
+        <div class="l--constrained">
+          <div class="row row--middled">
+            <div class="row__half">
+              <h2>Kumeu Valley Estate</h3>
+              <p>Located admist the vineyards of the Kumeu Valley just 20 minutes from Auckland city, the venue contains an outdoor ceremony area, a modernised farm house and a barn complete with an outdoor patio.</p>
+              <p>
+                It is located at <a href="https://www.google.co.nz/maps/place/Kumeu+Valley+Estate/@-36.74204,174.5089526,17z/data=!4m13!1m7!3m6!1s0x6d0d13e2a8bebcf9:0x72cb9cd0bdfe7b12!2s972+Old+N+Rd,+Huapai,+Waimauku+0882!3b1!8m2!3d-36.74204!4d174.5111413!3m4!1s0x6d0d13e2a8bebcf9:0x78e8ec38867eff09!8m2!3d-36.74204!4d174.5111413">972 Old North Road, Waimauku, Auckland</a>,
+                and more information and photos can be found on the <a href="https://www.kumeuvalley.co.nz/">Kumeu Valley Estate website</a>.
+              </p>
+            </div>
+            <div class="row__half">
+              <img src="images/kumeu.jpg" alt="Kumeu Valley Estate" />
+            </div>
+          </div>
         </div>
-        <div>
-          <h3>Day Trips From Auckland</h3>
-        </div>
-        <div>
-          <h3>Further Afoot</h3>
-        </div>
-      </div>
+      </section>
     </main>
   `
 }
 
 module.exports = home
+
+      //<section>
+      //  <div class="l--constrained">
+      //    <h2>Flight Information</h2>
+      //  </div>
+      //</section>
+      //<section>
+      //  <div class="l--constrained">
+      //    <h2>Car Rental</h2>
+      //  </div>
+      //</section>
+      //<section>
+      //  <div class="l--constrained">
+      //    <h2>Auckland Accomodation</h2>
+      //  </div>
+      //</section>
+      //<section>
+      //  <div class="l--constrained">
+      //    <h2>Things To Do</h2>
+      //    <div>
+      //      <h3>Around Auckland</h3>
+      //    </div>
+      //    <div>
+      //      <h3>Day Trips From Auckland</h3>
+      //    </div>
+      //    <div>
+      //      <h3>Further Afoot</h3>
+      //    </div>
+      //  </div>
+      //</section>
