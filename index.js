e1607113c6967259c0497aa6f5d4a9358074cf3b
Robert Pearce <me@robertwpearce.com>
2017-04-24 08:20:54 +1200

    simplified copying files to config

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 'use strict'
 
-const path = require('path')
+const { join } = require('path')
+const Task = require('data.task')
 const { minify } = require('html-minifier')
-const { compose } = require('ramda')
+const { chain, compose, map, sequence, unnest } = require('ramda')
 const { copy, remove, mkdirp, writeFile } = require('./lib/fs')
 const buildSass = require('./lib/sass')
-const {
-  outputDir,
-  sassDir,
-  sassEntry,
-  cssOutputPath,
-  cnamePath,
-  cnameOutputPath,
-  fontsPath,
-  fontsOutputPath,
-  imagesPath,
-  imagesOutputPath
-} = require('./config')
-
+const { copyPaths, outputDirectory, sourceDirectory } = require('./config')
 const homePage = require('./src/js/pages/home')
 
-const optimizeHtml = html => minify(html, { collapseWhitespace: true })
+// Directories
+const outputDir = join(__dirname, outputDirectory)
+const srcDir = join(__dirname, sourceDirectory)
 
+// Helpers
+const optimizeHtml = html => minify(html, { collapseWhitespace: true })
+const copyPath = p => copy(join(srcDir, p), join(outputDir, p))
+const copyAll = compose(
+  sequence(Task.of),
+  map(copyPath)
+)
+
+// Work!
 remove(outputDir)
   .chain(_ => mkdirp(outputDir))
-  .chain(_ => copy(cnamePath, cnameOutputPath))
-  .chain(_ => copy(fontsPath, fontsOutputPath))
-  .chain(_ => copy(imagesPath, imagesOutputPath))
-  .chain(_ => buildSass({ sassDir, sassEntry }))
-  .chain(css => writeFile(cssOutputPath, css))
-  .chain(_ => writeFile(path.join(outputDir, 'index.html'), optimizeHtml(homePage())))
+  .chain(_ => copyAll(copyPaths))
+  .chain(_ => buildSass(join(srcDir, 'sass'), join(srcDir, 'sass', 'app.scss')))
+  .chain(css => writeFile(join(outputDir, 'styles.css'), css))
+  .chain(_ => writeFile(join(outputDir, 'index.html'), optimizeHtml(homePage())))
   .fork(
     err => console.error('Error: ', err),
     data => console.log(data || 'Build succeeded.')
